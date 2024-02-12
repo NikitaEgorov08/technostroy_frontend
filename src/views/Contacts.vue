9357628e1d0b3a745306dffc62b0e2a6e5b02812
Nikita Egorov <n1ck.egorov@yandex.ru>
2024-02-12 16:03:38 +0300

    fix: contacts

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -8,13 +8,13 @@
     <div class="contacts-block">
       <div class="contact-info">
         <ContactCard
-          name="ООО “Торговый Дом “Челябинские строительные машины”"
-          jurAdd="Россия, 454012, г. Челябинск, Копейское шоссе, д.23"
-          jurInfo="ИНН 7449076417 ОГРН 1087449002783"
-          officeAdd="Адрес офиса: Россия, г. Челябинск, Копейское шоссе, д.23 "
-          tel1="+7 (919) 351-95-59"
-          tel2="+7 (919) 351-95-59"
-          mail="info@chelstroymash.ru"
+          :name="ooo"
+          :jurAdd="address"
+          :jurInfo="`ИНН ${inn} ОГРН ${ogrn}`"
+          :officeAdd="address"
+          :tel1="phone_1"
+          :tel2="phone_2"
+          :mail="email"
         />
       </div>
 
@@ -43,11 +43,35 @@ import ContactsForm from "@/components/Forms/ContactsForm.vue";
     ContactCard,
     ContactsForm,
   },
+  data() {
+    return {
+      phone_1: "",
+      phone_2: "",
+      inn: "",
+      ogrn: "",
+      email: "",
+      address: "",
+      ooo: "",
+    };
+  },
   methods: {
     back(e: Event) {
       e.preventDefault();
       this.$router.back();
     },
+  },
+  mounted() {
+    fetch("http://45.12.238.17:8000/api/contacts")
+      .then((res) => res.json())
+      .then((data) => {
+        this.phone_1 = data.phone_1;
+        this.phone_2 = data.phone_2;
+        this.inn = data.inn;
+        this.ogrn = data.ogrn;
+        this.email = data.email;
+        this.address = data.address;
+        this.ooo = data.ooo;
+      });
   },
 })
 export default class Contacts extends Vue {}
